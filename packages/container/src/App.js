123d3059f09a11afde69c3f33600397c89b740ad
Mirian Okradze <okradzemirian@gmail.com>
2021-10-16 16:25:51 +0400

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Container</h1>
+      <MarketingApp />
+    </div>
+  )
+}
+
+export default App
