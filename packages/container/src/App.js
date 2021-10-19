da2a642c53ff7d729bd8b018f71af74ac1875427
Mirian Okradze <okradzemirian@gmail.com>
2021-10-19 08:43:32 +0400

    Setup StyledProvider's generateClassName to avoid CSS name collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
 
