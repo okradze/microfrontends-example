daf542bbac22868d4adac16ae6ef4e5fe452ed82
Mirian Okradze <okradzemirian@gmail.com>
2021-10-23 21:18:32 +0400

    Unmount Dashboard App

--- packages/container/src/components/DashboardApp.js
+++ packages/container/src/components/DashboardApp.js
@@ -1,11 +1,18 @@
 import React, { useRef, useEffect } from 'react'
+import ReactDOM from 'react-dom'
 import { mount } from 'dashboard/DashboardApp'
 
 const DashboardApp = () => {
   const ref = useRef(null)
 
   useEffect(() => {
+    let current = ref.current
+
     mount(ref.current)
+
+    return () => {
+      ReactDOM.unmountComponentAtNode(current)
+    }
   }, [])
 
   return <div ref={ref} />
